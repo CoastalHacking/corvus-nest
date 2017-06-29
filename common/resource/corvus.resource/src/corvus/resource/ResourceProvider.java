df18abb249a81eae06213809709f7e871735a4e5
Jon Passki <jon@passki.us>
2017-06-29 13:28:24 -0700

    Resource provider
    Implemented the resource provider

--- common/resource/corvus.resource/src/corvus/resource/ResourceProvider.java
+++ common/resource/corvus.resource/src/corvus/resource/ResourceProvider.java
@@ -1,24 +1,30 @@
 package corvus.resource;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.parsley.config.Configurator;
+import org.eclipse.emf.parsley.resource.ResourceLoader;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
+// TODO: Test me!
 public class ResourceProvider implements Provider<Resource> {
 
 	@Inject
 	Configurator configurator;
 	
 	@Inject
+	ResourceLoader resourceLoader;
+	
+	@Inject
 	TransactionalEditingDomain ted;
 	
 	@Override
 	public Resource get() {
-//		ted.getResourceSet().getResource(uri, loadOnDemand)
-		return null;
+		URI uri = configurator.resourceURI(this);
+		return resourceLoader.getResource(ted, uri).getResource();
 	}
 
 }
